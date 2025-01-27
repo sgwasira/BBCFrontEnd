b8a27668b63d57040a9940590793ff2eaddc764c
sgwasira <sgwasira@yahoo.com>
2025-01-27 12:28:55 +0200

    Updated the pom file and created an extent.properties. Also updated the runner file

--- src/test/java/Runners/runner.java
+++ src/test/java/Runners/runner.java
@@ -3,10 +3,14 @@ package Runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(
         features = {"src/test/resources/Features"},
         glue = {"Steps"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = {
+                "pretty",
+                "json:target/cucumber-report/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         tags =  "@F1Results2023"
 )
 public class runner extends AbstractTestNGCucumberTests {
