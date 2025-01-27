d9eb36bf6277ab72a23e2f177f03969369d6f562
Shepherd <sgwasira@yahoo.com>
2025-01-27 12:03:57 +0200

    Update runner.java

--- src/test/java/Runners/runner.java
+++ src/test/java/Runners/runner.java
@@ -2,13 +2,11 @@ package Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/resources/Features"},
         glue = {"Steps"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        plugin = {pretty,"HTML:tatget/cucumber-Report.html","json:target/report.json"},
         tags =  "@F1Results2023"
 )
 public class runner extends AbstractTestNGCucumberTests {
