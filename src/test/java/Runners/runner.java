59a8644627889f7969b86f9cf4c35f958aead1bd
Shepherd <sgwasira@yahoo.com>
2025-01-27 12:00:22 +0200

    Update runner.java
    Added an new html report

--- src/test/java/Runners/runner.java
+++ src/test/java/Runners/runner.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/Features"},
         glue = {"Steps"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = {pretty,"HTML:tatget/cucumber-Report.html","json:target/report.json"},
         tags =  "@F1Results2023"
 )
 public class runner extends AbstractTestNGCucumberTests {
